74acdb684c32b9ab7c91041e5470101c1b82bfde
ANKIT NIRANJAN <43110505+ankitniranjan@users.noreply.github.com>
2020-11-04 11:58:33 +0530

    Update strongly_connected.py

--- Course-3: Algorithms on Graphs/week-2/strongly_connected.py
+++ Course-3: Algorithms on Graphs/week-2/strongly_connected.py
@@ -5,33 +5,43 @@ import numpy as np
 
 sys.setrecursionlimit(200000)
 
-def dfs(adj, visited, postOrder, clock, x):
+def dfs(adj, visited, postOrder,clock, x):
     visited[x] = True
     for i in adj[x]:
         if not visited[i]:
-            dfs(adj, visited, postOrder, clock, i)
+            dfs(adj,visited,postOrder,clock,i)      
     clock[0] += 1
-    postOrder[x] = clock[0]
+    postOrder[x] = clock[0]  
     return
 
-def number_of_strongly_connected_components(adj, adjr):
+def number_of_strongly_connected_components(adj,adjr):
     result = 0
     #write your code here
-    postOrder = [0] * len(adj)
-    visited = [False] * len(adj)
-    clock = [0]
 
+    
+
+    postOrder = [0] * len(adj)
+    #initialize visited with removed list
+    visited   =  [False] * len(adj)
+    clock = [0]
+    #find a sink by finding a source in the reverse graph
+    #a vertex in the source compoenent will have a higher postorder than
+    #those that are not sources
     for i in range(len(adjr)):
         if not visited[i]:
-            dfs(adjr, visited, postOrder, clock, i)
-
+            dfs(adjr,visited,postOrder,clock,i)
+    
+    #get reverse post order
     reversePostOrder = np.argsort(postOrder)[::-1]
 
+    #reinit visisted
+    visited   =  [False] * len(adj)
+    #run dfs on the real adjency list in reverse sort order.  count every time a dfs starts fresh from the top on an unvisited node
     for i in reversePostOrder:
         if not visited[i]:
             result += 1
-            dfs(adj, visited, postOrder, clock, i)
-
+            dfs(adj,visited,postOrder,clock,i)
+        
     return result
 
 if __name__ == '__main__':
@@ -43,7 +53,9 @@ if __name__ == '__main__':
     adj = [[] for _ in range(n)]
     for (a, b) in edges:
         adj[a - 1].append(b - 1)
+    #reverse graph
     adjr = [[] for _ in range(n)]
     for (a, b) in edges:
-        adjr[b -1].append(a - 1)
-    print(number_of_strongly_connected_components(adj, adjr))
+        adjr[b - 1].append(a - 1)
+
+    print(number_of_strongly_connected_components(adj,adjr))
